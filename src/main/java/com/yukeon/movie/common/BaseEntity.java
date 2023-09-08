a977cef78b216f80eb4d44de6793a0516b77b4b4
youKeon <yukeon97@gmail.com>
2023-09-08 23:10:41 +0900

    refactor : BaseEntity 수정

--- src/main/java/com/yukeon/movie/common/BaseEntity.java
+++ src/main/java/com/yukeon/movie/common/BaseEntity.java
@@ -4,17 +4,13 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
-@ToString
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedDate
